670a50457e9e10f3d4a70cc08340e9861e0d00b2
LiangQi <982856032@qq.com>
2020-06-22 15:02:13 +0800

    print the value in range of 1 to 200, instead of index

--- Fibonacci.java
+++ Fibonacci.java
@@ -1,19 +1,24 @@
 public class Fibonacci {
-    private static int[] val = new int[201];
+    //private static int[] val = new int[201];
 
     static int of(int index) {
-        if(index > 1) {
-            val[index] = val[index - 1] + val[index - 2];
-        }
-        return val[index];
+        // if(index > 1) {
+        //     val[index] = val[index - 1] + val[index - 2];
+        // }
+        // return val[index];
+
+        if (index == 1) {
+            return 1;
+        } else if (index == 2) {
+            return 1;
+        } else return of(index - 1) + of(index - 2);
     }
 
     public static void main(String[] args) {
-        val[0] = 0;
-        val[1] = 1;
-
-        for (int i = 1; i <= 200; i++) {
+        int i = 1;
+        while (of(i) <= 200) {
             System.out.println("Fibonacci.of(" + i + ") == " + Fibonacci.of(i));
+	    i++;
         }
     }
 }
